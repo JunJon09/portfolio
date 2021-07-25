58f02b970374f3f0f86e11aebe4adc782725e2c2
JunJon09 <84631911+JunJon09@users.noreply.github.com>
2021-07-25 11:10:55 +0900

    手直し

--- 01.js
+++ 01.js
@@ -14,14 +14,6 @@ function scrollHorizontally(e) {
 	//Math.maxはその中で大きいやつを返す。その逆
 	var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
 	// スクロールさせたいところ
-	//画像のピクセルと全体のピクセルをa,bで表す。
-	//そして、a/bで表した数字とdeltaでかける。
-	const image = new Image();
-	image.src = 'images/dog.png';
-	window.console.log(image.height);//2300
-	let client_w = document.getElementById('box').clientWidth;
-	let client_h = document.getElementById('box').clientHeight;	
-	window.console.log(client_h);//3200
 	delta = delta * 2.3;	
 	document.querySelector('article').scrollLeft -= delta; // Multiplied by 40
 	window.console.log(document.querySelector('article').scrollLeft -= delta);
